e7626949c8cc35f1fa157a96756d75dac8a015eb
Ryan008 <bluegx08@gmail.com>
2020-06-05 09:32:12 +0800

    Update layout.ejs

--- Week6/app/assets/js/all.js
+++ Week6/app/assets/js/all.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-  
+
   $( window ).resize(function() {
     if ($( window ).width() > 768) {
       $('#multiCollapseExample1').collapse('show')
@@ -7,5 +7,42 @@ $(document).ready(() => {
       $('#multiCollapseExample1').collapse('hide')
     }
   });
+
+
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    centeredSlides: false,
+    spaceBetween: null,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    }
+  });
+
+
+  // var mySwiper = new Swiper('.swiper-container', {
+  //   // Optional parameters
+  //   // direction: 'vertical',
+  //   loop: true,
+
+  //   // If we need pagination
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //   },
+
+  //   // Navigation arrows
+  //   navigation: {
+  //     // nextEl: '.swiper-button-next',
+  //     // prevEl: '.swiper-button-prev',
+  //     hideOnClick:true
+  //   },
+
+  //   // And if we need scrollbar
+  //   // scrollbar: {
+  //   //   el: '.swiper-scrollbar',
+  //   // },
+
+  
+  // })
 
 });
