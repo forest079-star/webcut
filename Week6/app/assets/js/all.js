e7ba3754b25118f3e3ec4d8afbaf99fd4e5b4093
Ryan008 <bluegx08@gmail.com>
2020-06-12 18:30:51 +0800

    add product, detail page

--- Week6/app/assets/js/all.js
+++ Week6/app/assets/js/all.js
@@ -11,38 +11,26 @@ $(document).ready(() => {
 
   var swiper = new Swiper('.swiper-container', {
     slidesPerView: 'auto',
-    centeredSlides: false,
+    centeredSlides: true,
     spaceBetween: null,
     pagination: {
       el: '.swiper-pagination',
       clickable: true
-    }
+    },
   });
 
-
-  // var mySwiper = new Swiper('.swiper-container', {
-  //   // Optional parameters
-  //   // direction: 'vertical',
-  //   loop: true,
-
-  //   // If we need pagination
-  //   pagination: {
-  //     el: '.swiper-pagination',
-  //   },
-
-  //   // Navigation arrows
-  //   navigation: {
-  //     // nextEl: '.swiper-button-next',
-  //     // prevEl: '.swiper-button-prev',
-  //     hideOnClick:true
-  //   },
-
-  //   // And if we need scrollbar
-  //   // scrollbar: {
-  //   //   el: '.swiper-scrollbar',
-  //   // },
-
-  
-  // })
+  var swiper = new Swiper('.swiper-container-guess', {
+    slidesPerView: 'auto',
+    // centeredSlides: true,
+    spaceBetween: null,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    loop: true,
+    // autoplay: {
+    //   delay: 3000,
+    // },
+  });
 
 });
