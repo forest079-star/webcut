c0ba2516ff93aa512e5ae76952a47016e9e525ad
forest079 <bluegx08@gmail.com>
2020-07-04 01:54:41 +0800

    add

---
+++ Week6/dist/assets/js/all.js
@@ -0,0 +1,35 @@
+"use strict";
+
+$(document).ready(function () {
+  $(window).resize(function () {
+    if ($(window).width() > 768) {
+      $('#multiCollapseExample1').collapse('show');
+    } else {
+      $('#multiCollapseExample1').collapse('hide');
+    }
+  });
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    spaceBetween: null,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: false
+    },
+    loop: true
+  });
+  var swiper = new Swiper('.swiper-container-guess', {
+    slidesPerView: 'auto',
+    // centeredSlides: true,
+    spaceBetween: null,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    loop: true // autoplay: {
+    //   delay: 3000,
+    // },
+
+  });
+});
+//# sourceMappingURL=all.js.map
